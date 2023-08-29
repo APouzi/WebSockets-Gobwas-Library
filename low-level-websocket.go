e5d21698eddfd77b15ce4b2e571ac834fe069ff1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-29 18:21:49 +0100

    added the low-level reading from client to server -AP

---
+++ low-level-websocket.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+
+	"github.com/gobwas/ws"
+)
+
+func WebSocketLowLevel(w http.ResponseWriter, r *http.Request) {
+	// var conn net.Conn
+	
+	conn, wr, _, err  := ws.UpgradeHTTP(r, w)
+	if err != nil{
+		panic(err)
+	}
+	defer conn.Close()
+	bufReader := bufio.NewReaderSize(wr.Reader, 300)
+	for {
+		header, err := ws.ReadHeader(wr.Reader)
+		if err != nil{
+			fmt.Println("header error:", err)
+			continue
+		}
+		payload := make([]byte, header.Length)
+		
+		bufReader.Read(payload)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		ws.Cipher(payload,header.Mask,0)
+		fmt.Println(string(payload))
+		
+	}
+}
