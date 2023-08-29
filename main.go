5abb281b18ddcef110032c5c0bafc8799ca2e8fb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-29 02:01:20 +0100

    using a simplified version of using gobwas with wsutils -AP

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
 
 func main() {
 	fmt.Println("Starting GobWas Server")
+	http.HandleFunc("/ws", WebSocket)
+	err :=  http.ListenAndServe(":8080", nil)
+	if err != nil{
+		fmt.Println("error is yeah", err)
+		return
+	}
+	
+
+}
+
+func WebSocket (writer http.ResponseWriter, request *http.Request){
+	// writer.Write([]byte("hello"))
+	conn, _, _, err := ws.UpgradeHTTP(request, writer)
+	if err != nil{
+		fmt.Println(err)
+	}
+	for {
+		msg, op, err := wsutil.ReadClientData(conn)
+		if err != nil{
+			fmt.Println("ReadClient error")
+			return
+		}
+
+		err = wsutil.WriteServerMessage(conn, op, msg)
+		if err != nil{
+			fmt.Println("WriteServer error")
+			return
+		}
+	}
 }
