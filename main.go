3a4e5048e5ab892efb16fba44629363c1fe670a5
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-29 04:04:49 +0100

    added a stream of data into two concurrent coroutines, one that listens and another that streams data into the client -AP

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
@@ -11,6 +13,8 @@ import (
 func main() {
 	fmt.Println("Starting GobWas Server")
 	http.HandleFunc("/ws", WebSocket)
+	http.HandleFunc("/ws-stream", WebSocketStreamToClient)
+	// http.HandleFunc("/ws-low-level",WebSocket2)
 	err :=  http.ListenAndServe(":8080", nil)
 	if err != nil{
 		fmt.Println("error is yeah", err)
@@ -21,7 +25,6 @@ func main() {
 }
 
 func WebSocket (writer http.ResponseWriter, request *http.Request){
-	// writer.Write([]byte("hello"))
 	conn, _, _, err := ws.UpgradeHTTP(request, writer)
 	if err != nil{
 		fmt.Println(err)
@@ -32,6 +35,10 @@ func WebSocket (writer http.ResponseWriter, request *http.Request){
 			fmt.Println("ReadClient error")
 			return
 		}
+		if string(msg) == "end"{
+			conn.Close()
+			return
+		}
 
 		err = wsutil.WriteServerMessage(conn, op, msg)
 		if err != nil{
@@ -39,4 +46,42 @@ func WebSocket (writer http.ResponseWriter, request *http.Request){
 			return
 		}
 	}
+	// writer.Write([]byte("hello"))
+}
+
+func WebSocketStreamToClient (writer http.ResponseWriter, request *http.Request){
+	conn, _, _, err := ws.UpgradeHTTP(request, writer)
+	if err != nil{
+		fmt.Println(err)
+	}
+	go func (conn net.Conn) {
+		defer conn.Close()
+		for {
+			msg, op, err := wsutil.ReadClientData(conn)
+			fmt.Println("op",op)
+			if err != nil{
+				fmt.Println(err)
+				fmt.Println("ReadClient error")
+				return
+			}
+			if string(msg) == "end"{
+				return
+			}
+		}
+	}(conn)
+
+	go func (conn net.Conn) {
+		defer conn.Close()
+		for {
+			msg := []byte("hello over and over")
+			err = wsutil.WriteServerMessage(conn, ws.OpCode(1), msg)
+			if err != nil{
+				fmt.Println("WriteServer error")
+				return
+			}
+			time.Sleep(time.Second *1)
+		}
+	}(conn)
+	
+	// writer.Write([]byte("hello"))
 }
